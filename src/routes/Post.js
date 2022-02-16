37528063dc54e3b9368f7e22227a720f400b1af4
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2022-02-16 20:06:52 -0300

    feature - rotas para os endpoints de post, implementados

---
+++ src/routes/Post.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+
+router.post("/", (req, res) => {
+  res.send("Aqui vai o controller de Post, para o POST");
+});
+
+router.get("/", (req, res) => {
+  res.send("Aqui vai o controller de Post, para o GET");
+});
+
+router.get("/:id", (req, res) => {
+  res.send("Aqui vai o controller de Post através do :id, para o GET");
+});
+
+router.put("/:id", (req, res) => {
+  res.send("Aqui vai o controller de Post através do :id, para o PUT");
+});
+
+router.get("/search?q=:searchTerm", (req, res) => {
+  res.send("Aqui vai o controller de Post através do searchTerm, para o GET");
+});
+
+router.delete("/:id", (req, res) => {
+  res.send("Aqui vai o controller de Post através do :id, para o DELETE");
+});
+
+module.exports = router;
