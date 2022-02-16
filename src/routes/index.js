c6c709f13cb05dbd8ede54b8740bb308d59d23ed
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2022-02-16 20:09:41 -0300

    feature - rotas organizadas no index de routes

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const UserRoutes = require("./User");
+const LoginRoutes = require("./Login");
+const PostRoutes = require("./Post");
+
+module.exports = {
+  UserRoutes,
+  LoginRoutes,
+  PostRoutes,
+};
