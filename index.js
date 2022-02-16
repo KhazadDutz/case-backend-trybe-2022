e4f63030699c1903ad9f6a3a893e3aab2d58a835
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2022-02-16 15:55:10 -0300

    Rota para a home pré-definida

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use("/", (request, response) => {
+  response.send("Aqui vai a Home Page da Aplicação");
+});
+
 app.listen(PORT, () => console.log(`Aplicação rodando na porta ${PORT}`));
