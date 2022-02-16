a5bb6336d5a25b01ad7b7d6466adacea418220f4
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2022-02-16 15:51:33 -0300

    Arquivo index criado

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`Aplicação rodando na porta ${PORT}`));
