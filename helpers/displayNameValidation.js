7b8615af8d1264382b0277381fcd06b0617100df
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2022-02-19 19:03:53 -0300

    feature - tentativa de implementar testes

--- helpers/displayNameValidation.js
+++ helpers/displayNameValidation.js
@@ -1,15 +1,9 @@
 const { customException } = require("./customException");
 
 const validateDisplayName = (displayName) => {
-  if (displayName === undefined)
-    throw customException(400, '"displayName" is required');
-  if (displayName === "")
-    throw customException(400, '"displayName" is not allowed to be empty');
+  if (displayName === undefined) throw customException(400, '"displayName" is required');
   if (displayName.length < 8)
-    throw customException(
-      400,
-      '"displayName" must be at least 8 characters long'
-    );
+    throw customException(400, '"displayName" must be at least 8 characters long');
   return null;
 };
 
